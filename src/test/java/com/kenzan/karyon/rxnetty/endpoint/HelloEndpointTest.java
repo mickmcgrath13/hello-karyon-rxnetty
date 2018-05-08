ecad0cb827bce92d527b2299044cb41118a80be6
Mick McGrath <mick.mc.13@gmail.com>
2018-05-08 10:32:11 -0400

    1.3.2

--- src/test/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpointTest.java
+++ src/test/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpointTest.java
@@ -50,6 +50,6 @@ public class HelloEndpointTest {
 
         Observable<String> hello = helloEndpoint.getHelloName(request);
 
-        Assert.assertEquals("Yo, name! This is a test (1.3.1).", hello.toBlocking().first());
+        Assert.assertEquals("Yo, name! This is a test (1.3.2).", hello.toBlocking().first());
     }
 }
